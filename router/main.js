bf2fa8ca00129a1f2ae2e14f5f099944247b730d
namho1404 <skagh1404@gmail.com>
2018-05-04 10:27:38 +0900

    add visual page

--- router/main.js
+++ router/main.js
@@ -6,13 +6,18 @@ module.exports = function(app)
     app.get('/about',function(req,res){
         res.render('about.html');
     });
+    /*
     app.get('/visualizer',function(req,res){
         res.render('visualizer.html');
     });
     app.get('/dummy',function(req,res){
         res.render('dummy.html');
     });
+    */
     app.get('/dual',function(req,res){
         res.render('dual.html');
     });
+    app.get('/visual',function(req,res){
+        res.render('visual.html')
+    })
 }
