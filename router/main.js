9bfc27dffd8d060041b2ed56a275d20e4f990083
namho1404 <skagh1404@gmail.com>
2018-05-10 00:48:38 +0900

    add test page

--- router/main.js
+++ router/main.js
@@ -3,21 +3,10 @@ module.exports = function(app)
     app.get('/',function(req,res){
         res.render('dual.html')
     });
-    app.get('/about',function(req,res){
-        res.render('about.html');
-    });
-    /*
-    app.get('/visualizer',function(req,res){
-        res.render('visualizer.html');
-    });
-    app.get('/dummy',function(req,res){
-        res.render('dummy.html');
-    });
-    */
-    app.get('/dual',function(req,res){
-        res.render('dual.html');
-    });
     app.get('/visual',function(req,res){
         res.render('visual.html')
+    });
+    app.get('/temp',function(req,res){
+        res.render('temp.html')
     });
 }
