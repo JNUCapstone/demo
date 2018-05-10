c8b0703335b490fedce405163f4a9e523af433bc
namho1404 <skagh1404@gmail.com>
2018-05-10 21:11:10 +0900

    apply nav

---
+++ public/script.js
@@ -0,0 +1,3 @@
+function changesrc(text) {
+    document.getElementById("iframe1").src = text;
+}
--- router/main.js
+++ router/main.js
@@ -1,12 +1,15 @@
 module.exports = function(app)
 {
     app.get('/',function(req,res){
-        res.render('dual.html')
+        res.render('index.html')
     });
     app.get('/visual',function(req,res){
         res.render('visual.html')
     });
     app.get('/temp',function(req,res){
         res.render('temp.html')
+    });
+    app.get('/visualizer',function(req,res){
+        res.render('visualizer.html')
     });
 }
