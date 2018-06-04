2145ebf0a2903b8b9584d1bd436a7e534d0ddf73
namho1404 <skagh1404@gmail.com>
2018-06-05 03:05:28 +0900

    add visualizer3 rename visualizers

--- router/main.js
+++ router/main.js
@@ -4,13 +4,17 @@ module.exports = function(app)
         console.log("home page");
         res.render('index.html')
     });
-    app.get('/visual',function(req,res){
-        console.log("visual page");
-        res.render('visual.html')
+    app.get('/visualizer1',function(req,res){
+        console.log("visualizer1 page");
+        res.render('visualizer1.html')
     });
-    app.get('/visualizer',function(req,res){
-        console.log("visualizer page");
-        res.render('visualizer.html')
+    app.get('/visualizer2',function(req,res){
+        console.log("visualizer2 page");
+        res.render('visualizer2.html')
+    });
+    app.get('/visualizer3',function(req,res){
+        console.log("visualizer page3");
+        res.render('visualizer3.html')
     });
     app.get('/dummy',function(req,res){
         console.log("dummy page");
@@ -27,33 +31,5 @@ module.exports = function(app)
     app.get('/3',function(req,res){
         console.log("3 page");
         res.render('3.html')
-    });
-    app.get('/4',function(req,res){
-        console.log("4 page");
-        res.render('4.html')
-    });
-    app.get('/5',function(req,res){
-        console.log("5 page");
-        res.render('5.html')
-    });
-    app.get('/6',function(req,res){
-        console.log("6 page");
-        res.render('6.html')
-    });
-    app.get('/7',function(req,res){
-        console.log("7 page");
-        res.render('7.html')
-    });
-    app.get('/8',function(req,res){
-        console.log("8 page");
-        res.render('8.html')
-    });
-    app.get('/9',function(req,res){
-        console.log("9 page");
-        res.render('9.html')
-    });
-    app.get('/10',function(req,res){
-        console.log("10 page");
-        res.render('10.html')
     });
 }
