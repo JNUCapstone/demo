aa91d82d4634e9a2b9d6e401891431384a4bc816
namho1404 <skagh1404@gmail.com>
2018-06-08 20:29:05 +0900

    a

--- router/main.js
+++ router/main.js
@@ -12,28 +12,8 @@ module.exports = function(app)
         console.log("visualizer2 page");
         res.render('visualizer2.html')
     });
-    app.get('/visualizer3',function(req,res){
-        console.log("visualizer page3");
-        res.render('visualizer3.html')
-    });
-    app.get('/visualizer4',function(req,res){
-        console.log("visualizer page4");
-        res.render('visualizer4.html')
-    });
     app.get('/dummy',function(req,res){
         console.log("dummy page");
         res.render('dummy.html')
-    });
-    app.get('/1',function(req,res){
-        console.log("1 page");
-        res.render('1.html')
-    });
-    app.get('/2',function(req,res){
-        console.log("2 page");
-        res.render('2.html')
-    });
-    app.get('/3',function(req,res){
-        console.log("3 page");
-        res.render('3.html')
     });
 }
